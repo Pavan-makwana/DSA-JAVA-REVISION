a1d0e374bbc4d3e95e8a34233502cdc003d4d656
Pavan <pavan561981@gmail.com>
2025-12-03 19:20:08 +0530

    deleted example file

--- Arrays/ExampleArrays.java
+++
@@ -1,9 +0,0 @@
-// Example problem in Arrays
-// Author: Pavan Makwana
-// Date: Nov 2025
-
-public class ExampleArrays {
-    public static void main(String[] args) {
-        System.out.println("Hello from Arrays problems!");
-    }
-}
