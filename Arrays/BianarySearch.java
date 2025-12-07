ab6046006bb161c4c806eca4c0af53d4f4453193
Pavan <pavan561981@gmail.com>
2025-12-07 13:13:57 +0530

    BinarySearch

--- Arrays/BianarySearch.java
+++ Arrays/BianarySearch.java
@@ -1,17 +1,20 @@
 public class BianarySearch {
     public static int binarySearch(int[] numbers, int key) {
-        int start = 0, end = numbers.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
+        int start =0, end = numbers.length -1 ;
+        while (start <=  end ) {
+            int mid = (start + end ) /2;
+            // if key is Mid
             if (numbers[mid] == key) {
                 return mid;
             }
-            if (numbers[mid] < key) { // right half condition
-                start = mid + 1;
-            } else { // left half condition
-                end = mid - 1;
+
+            //  if key is in right
+            if (numbers[mid] < key) {
+                start = mid +1 ;
+            }else{ // Key is in left
+                end = mid -1;
             }
+
         }
         return -1;
     }
