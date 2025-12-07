b5a10c38b5832675e96eb38561aab100e929a942
Pavan <pavan561981@gmail.com>
2025-12-07 13:06:25 +0530

    LinearSearch

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,22 +1,15 @@
-
-
-public class LinearSearch {
-    public static int linearSearch(int [] numbers,int key ) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == key) {
-                return i;
+public class LinearSearch{
+    public static int linearSearch(int num[] ,int key ){
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == key) {
+                return i+1;
             }
         }
         return 0;
     }
     public static void main(String[] args) {
-        int numbers[] = {2,4,5,8,25,3,53,32};
-        int key = 3;
-        int idx = linearSearch(numbers, key);
-        if (idx ==0) {
-            System.out.println("key is not in the Array " );
-        } else {
-            System.out.println("key is at " + idx );
-        }
+        int num[] = {1,34,5,32,56,20};
+        int key = 20;
+        System.out.println("Index of The Key :"+linearSearch(num,key));
     }
 }
