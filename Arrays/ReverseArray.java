59e30477fb2cf9b0425bc909d0ec2a300c78a750
Pavan <pavan561981@gmail.com>
2025-12-09 18:41:57 +0530

    Reverse Array

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,24 +1,21 @@
 public class ReverseArray {
-
-    public static void reverseArray(int num []) {
-        int first = 0, last = num.length -1 ;
+    public static void reverseArr(int[] nums) {
+        int first = 0, last = nums.length - 1;
         while (first < last) {
-            // swap
-            int temp = num[last];
-            num[last] = num[first];
-            num[first]= temp;
-            
+            int temp = nums[last];
+            nums[last] = nums[first];
+            nums[first] = temp;
             first++;
             last--;
-
         }
+
     }
 
     public static void main(String[] args) {
-        int numbers [] = {1,2,3,4,5};
-        reverseArray(numbers);
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        int nums[] = { 1, 4, 5, 6, 4, 5, 2 };
+        reverseArr(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
         }
     }
 }
